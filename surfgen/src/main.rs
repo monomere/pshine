e9c3526d1271af5db1555ec915e1f82d501a287b
monomere <monomere@duck.com>
2025-04-08 12:11:04 +0300

    crater noise, but its bad, wip

--- surfgen/src/main.rs
+++ surfgen/src/main.rs
@@ -1,140 +1,127 @@
-use core::f32;
+#![feature(slice_as_chunks)]
 
-trait Projection {
-	type Output;
-	fn samples(&self) -> impl Iterator<Item = (Self::Output, vek::Vec3<f32>)>;
-}
+use core::{f32, f64};
+use noise::utils::NoiseMapBuilder;
 
-struct Cylindrical {
-	samples_y: i32,
-	samples_x: i32,
-}
-
-impl Projection for Cylindrical {
-	type Output = vek::Vec2<i32>;
-
-	fn samples(&self) -> impl Iterator<Item = (Self::Output, vek::Vec3<f32>)> {
-		let half_y = self.samples_y / 2;
-		let y_coeff: f32 = f32::consts::FRAC_PI_2 / half_y as f32;
-		let half_x = self.samples_x / 2;
-		let x_coeff: f32 = f32::consts::PI / half_x as f32;
-		(-half_y..half_y).flat_map(move |y| (-half_x..half_x).map(move |x| {
-			let lat = y as f32 * y_coeff;
-			let lon = x as f32 * x_coeff;
-			let v = vek::Vec3::new(
-				f32::cos(lat) * f32::cos(lon),
-				f32::cos(lat) * f32::sin(lon),
-				f32::sin(lat)
-			);
-			(vek::Vec2::new(x + half_x, y + half_y), v)
-		}))
-	}
-}
-
-fn gather_to_image<C, P>(
-	out: &mut impl image::GenericImage<Pixel = P>,
-	proj: &impl Projection<Output = vek::Vec2<C>>,
-	conv: &impl Fn(f64) -> P,
-	n: &impl noise::NoiseFn<f64, 3>,
-) where C: num_traits::int::PrimInt + num_traits::AsPrimitive<u32> {
-	for (latlon, p) in proj.samples() {
-		out.put_pixel(latlon.x.as_(), latlon.y.as_(),
-			conv(n.get(p.as_().into_tuple().into())));
-	}
-}
-
-fn u8_of_unorm(v: f32) -> u8 {
-	(v.clamp(0.0, 1.0) * 255.0) as u8
-}
-
-fn grayscale(v: u8) -> image::Rgb<u8> {
-	image::Rgb([v, v, v])
-}
-
-fn vekrgb_f32_to_u8(v: vek::Rgb<f32>) -> image::Rgb<u8> {
-	image::Rgb([
-		u8_of_unorm(v.r),
-		u8_of_unorm(v.g),
-		u8_of_unorm(v.b),
-	])
-} 
-
-fn color_palette(
-	x: f32
-) -> vek::Rgb<f32> {
-	let _c = |r, g, b| vek::Rgb::<f32>::new(r, g, b);
-	let begin = _c(0.10, 0.08, 0.04);
-	let ranges = [
-		(0.1, _c(0.21, 0.20, 0.11)),
-		(0.3, _c(0.41, 0.35, 0.21)),
-		(0.6, _c(0.61, 0.65, 0.51)),
-		(0.8, _c(0.78, 0.82, 0.79)),
-	];
-	let end = _c(0.87, 0.88, 0.81);
-	let part = ranges.partition_point(|v| v.0 <= x);
-	let ((at, a), (bt, b)) = if part == 0 {
-		((0.0, begin), ranges[0])
-	} else if part == ranges.len() {
-		(ranges[ranges.len() - 1], (1.0, end))
-	} else {
-		(ranges[part - 1], ranges[part])
-	};
-	vek::Vec3::lerp(a.into(), b.into(),
-		(x - at) / (bt - at)).into()
-}
-
-struct CraterNoise {
+struct CraterNoise<_W: noise::NoiseFn<f64, 3>> {
 	seed: u32,
+	crater_noise_distance: _W,
+	crater_noise_value: _W,
 }
 
-trait IntoTuple<T> {
-	type Output;
-	fn into_tuple(self) -> Self::Output;
-}
-
-impl<T> IntoTuple<T> for [T; 3] {
-	type Output = (T, T, T);
-
-	fn into_tuple(self) -> Self::Output {
-		let [a, b, c] = self;
-		(a, b, c)
-	}
-}
-
-impl noise::NoiseFn<f64, 3> for CraterNoise {
+impl<_W: noise::NoiseFn<f64, 3>> noise::NoiseFn<f64, 3> for CraterNoise<_W> {
 	fn get(&self, point: [f64; 3]) -> f64 {
 		use vek::Vec3;
 		type Vec3f32 = Vec3<f32>;
 		type Vec3f64 = Vec3<f64>;
 
 		let p: Vec3f32 = Vec3f64::from(point).as_();
-		// TODO: voronoi noise, select crater center, generate crater
-		//       with polar coordinates around the crater center.
-		0.0
+		let v = self.crater_noise_value.get(point);
+		let d = self.crater_noise_distance.get(point) * 0.5 + 0.5
+			+ v * 0.2;
+		let l = 0.3;
+		if d < l && v < 0.0 {
+			d / l  - 0.5
+		} else {
+			0.0
+		}
 	}
 }
 
-impl CraterNoise {
-	fn new(seed: u32) -> Self {
-		Self { seed }
+fn make_noise_scale(
+	val_bias: f64,
+	val_scale: f64,
+	pnt_scale: f64,
+	base: impl noise::NoiseFn<f64, 3>,
+) -> impl noise::NoiseFn<f64, 3> {
+	noise::ScaleBias::new(
+		noise::ScalePoint::new(base)
+			.set_scale(pnt_scale))
+		.set_scale(val_scale)
+		.set_bias(val_bias)
+}
+
+fn make_const_displacement(
+	off: vek::Vec3<f64>,
+	base: impl noise::NoiseFn<f64, 3>,
+) -> impl noise::NoiseFn<f64, 3> {
+	noise::Displace::new(base,
+		noise::Constant::new(off.x),
+		noise::Constant::new(off.y),
+		noise::Constant::new(off.z),
+		noise::Constant::new(0.0))
+}
+
+
+fn make_displacement(
+	seed: u32,
+	base: impl noise::NoiseFn<f64, 3>,
+) -> impl noise::NoiseFn<f64, 3> {
+	noise::Displace::new(base,
+		make_noise_scale(
+			0.0, 0.5, 0.2,
+			noise::Perlin::new(seed.wrapping_mul(69))),
+		make_noise_scale(
+			0.0, 0.5, 0.2,
+			noise::Perlin::new(seed.wrapping_mul(123))),
+		make_noise_scale(
+			0.0, 0.5, 0.2,
+			noise::Perlin::new(seed.wrapping_mul(231))),
+		make_noise_scale(
+			0.0, 0.5, 0.2,
+			noise::Perlin::new(seed.wrapping_mul(321))))
+}
+
+fn new_crater_noise(seed: u32) -> impl noise::NoiseFn<f64, 3> {
+	use noise::core::worley::ReturnType;
+	let base = noise::Worley::new(seed)
+		.set_distance_function(
+		noise::core::worley::distance_functions::euclidean)
+		.set_frequency(5.0);
+	let off = vek::Vec3::new(453.0, 1213.2, 922.0);
+	CraterNoise {
+		seed,
+		crater_noise_distance:
+			make_const_displacement(off,
+				make_displacement(seed, base.clone()
+					.set_return_type(ReturnType::Distance))),
+		crater_noise_value:
+			make_const_displacement(off,
+				make_displacement(seed, base.clone()
+					.set_return_type(ReturnType::Value))),
 	}
 }
 
 fn main() {
-	let cyl = Cylindrical {
-		samples_x: 2048,
-		samples_y: 1024,
-	};
-	let mut buf = image::ImageBuffer::new(cyl.samples_x as u32, cyl.samples_y as u32);
-	gather_to_image(&mut buf, &cyl, &|v|
-		vekrgb_f32_to_u8(color_palette(v as f32)),
-		&noise::Add::new(
-			noise::ScaleBias::new(noise::ScalePoint::new(noise::Fbm::<noise::Perlin>
-				::new(123123123))
-			.set_scale(2.0)).set_scale(0.5).set_bias(0.5),
-			CraterNoise::new(69420 /* haha */)));
+	let final_noise =
+	noise::Add::new(
+		noise::ScaleBias::new(noise::ScalePoint::new(noise::Fbm::<noise::Perlin>
+			::new(123123123))
+		.set_scale(2.0)).set_scale(0.5).set_bias(0.5),
+		new_crater_noise(69420 /* haha */)
+		);
+
+	let noise_map = noise::utils::SphereMapBuilder::new(final_noise)
+		.set_bounds(-90.0, 90.0, -180.0, 180.0)
+		.set_size(2048, 1024)
+		.build();
+
+	let noise_image = noise::utils::ImageRenderer::new()
+		.set_gradient(noise::utils::ColorGradient::new()
+			.add_gradient_point(-1.0, [82, 80, 79, 255])
+			.add_gradient_point(0.5, [128, 121, 110, 255])
+			.add_gradient_point(1.1, [198, 185, 180, 255]))
+		.render(&noise_map);
 	
+	let mut buf = image::RgbImage::new(
+		noise_image.size().0 as _,
+		noise_image.size().1 as _,
+	);
 	let out = std::fs::File::create("output.png").unwrap();
 	let mut wr = std::io::BufWriter::new(out);
+	for (i, v) in noise_image.iter().enumerate() {
+		buf.as_flat_samples_mut().as_mut_slice()
+			.as_chunks_mut::<3>().0[i] = [v[0], v[1], v[2]];
+	}
 	buf.write_to(&mut wr, image::ImageFormat::Png).unwrap();
 }
